5deb71cc14958efa77b8fb6040522647f4c12013
aqiu04 <allenqiu04@gmail.com>
2024-10-13 19:11:14 -0700

    add socials

---
+++ src/app/config.ts
@@ -0,0 +1,8 @@
+
+
+export const socials = {
+    linkedin: "https://www.linkedin.com/in/allen-qiu-541243226/",
+    github: "https://github.com/aqiu04",
+    instagram: "https://www.instagram.com/aqiu04/",
+    email: "mailto:allenqiu04@gmail.com",
+}
